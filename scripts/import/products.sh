0b3f20705633a7465627c5709dcac075294bc0a5
rakshyakc <rakshya.khatri1@gmail.com>
2018-12-19 09:46:24 -0500

    Updated product loading script and reversed

--- scripts/import/products.sh
+++ scripts/import/products.sh
@@ -1,1 +1,1 @@
-mongoimport --db=Squad-project-api-development --collection=products --type=csv --headerline --file=data/products.csv
+mongoimport --db=Squad-project-api-development --collection=products --drop --type=csv --headerline --file=data/products.csv
